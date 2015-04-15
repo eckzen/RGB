c4e1074754af6b306de4dca61548b4ac5995f2fa
eckzen <eckzen@yahoo.com>
2015-04-15 15:07:31 +0800

    Move the nav bar inside the sidebar

--- sidebar.php
+++ sidebar.php
@@ -10,6 +10,11 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
+<nav id="site-navigation" class="main-navigation" role="navigation">
+					
+	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+</nav><!-- #site-navigation -->
+
 <div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
